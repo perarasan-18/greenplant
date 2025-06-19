017adaf46a19c30a1962fe041ccae036a4813757
perarasan-18 <perarasanvk04@gmail.com>
2025-06-19 09:54:28 +0530

    updated js

--- home.js
+++
@@ -1,9 +0,0 @@
-var threebar=document.getElementById("threebar")
-var sidenavbar=document.getElementById("sidenavbar")
-var closebar=document.getElementById("close-sidebar")
-threebar.addEventListener("click",function(){
-    sidenavbar.style.right="0"
-})
-closebar.addEventListener("click",function(){
-    sidenavbar.style.right="-35%"
-})
